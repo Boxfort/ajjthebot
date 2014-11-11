d71d7b94b2ae7cb8263280204eb12802010f0f2b
Sam Gaus <git@gaus.ie>
2014-11-11 17:45:10 +0000

    Updated GetLyrics to work for AJJ

--- GetLyrics.py
+++ GetLyrics.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env/python
 
+# License below inherited from parent project TMBOTG - Sam Gaus
+#
 # Copyright (c) 2013 Brett g Porter
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -26,7 +28,7 @@ from urllib import unquote
 from os.path import join
 import requests
 
-kBaseUrl = "http://tmbw.net"
+kBaseUrl = "http://lyrics.wikia.com/Andrew_Jackson_Jihad"
 
 kOutputDir = "data"
 
@@ -36,7 +38,7 @@ def Log(s):
 
 def Scrub(s):
    ''' Do whatever cleanup we need to do to turn a string (possibly with spaces
-      in it) to a space-less name more usable as a file name 
+      in it) to a space-less name more usable as a file name
    '''
    kIllegalChars = "!@#$%^&*()/\\{}[];:,?~`|"
    s = s.strip()
@@ -60,65 +62,47 @@ def GetSoup(urlFragment):
 
 
 def ProcessDiscography(url):
-   ''' load the page at 'url' and process everything in the table of albums 
+   ''' load the page at 'url' and process everything in the table of albums
       (with the id 'discog'), and in turn process each album.
    '''
    soup = GetSoup(url)
-   table = soup.find(id="discog")
-   rows = table("tr")
-   # (skip the table header...)
-   for row in rows[1:]:
-      cells = row("td")
-      link = cells[1].a
-      name = link.text
-      urlFragment = link['href']
-      Log("Handling album '{0}'".format(name))
-      ProcessAlbum(name, urlFragment)
+   for album in soup.find_all("ol"):
+      if len(album.contents) == 0:
+         continue
+      albumName = album.find_previous_sibling("h2").find("span", {"class":"mw-headline"}).get_text()
+      for list_item in album.contents:
+         song = list_item.find("a")
+         name = song.text
+         urlFragment = song['href']
 
-
-def ProcessAlbum(albumName, url):
-   ''' look for the table on an album page that contains the track
-      listing for the album. This is a little more complicated because there
-      may be multiple track listings associated with a single album (alternate release versions,
-      etc.) so we can't rely on looking for a specific id. The tables that contain track
-      data all have the class name 'ebena', so we'll look for tags with that class 
-      and ignore any that end up not containing links to pages of lyrics.
-
-      Hackier than I like, but we're scraping HTML and shouldn't be surprised.
-   '''
-   soup = GetSoup(url)
-   tables = soup.find_all(attrs={"class": "ebena"})
-
-   for table in tables:
-      rows = table("tr")
-      for row in rows:
-         cells = row("td")
-         # the "Purchase" table has fewer columns than the track info table. 
-         if 5 == len(cells):
-            try:
-               trackName = cells[1].a.text
-               lyricUrl = cells[3].a['href']
-               ProcessTrack(albumName, trackName, lyricUrl)
-            except Exception, e:
-               print str(e)
-
+         ''' If the page does not exist, Wikia will give the link a "new" class. '''
+         if not song.has_key("class") or "new" not in song["class"]:
+            Log("Handling song '{0}'".format(name))
+            ProcessTrack(albumName, name, urlFragment)
+         else:
+            Log("Skipping '{0}' because lyrics do not exist".format(name))
 
 
 def ProcessTrack(albumName, trackName, url):
    '''
-      url points at a lyrics page. The lyrics are inside a <div> that has the 
-      class 'lyrics-table'.
+      url points at a lyrics page. The lyrics are inside a <div> that has the
+      class 'lyricbox'.
    '''
    soup = GetSoup(url)
-   lyrics = soup.find(attrs={"class": "lyrics-table"})
-   stanzas = lyrics.find_all("p")
+   lyrics = soup.find(attrs={"class": "lyricbox"})
+
+   for script in lyrics(["script", "style"]):
+      script.extract()
+
+   for br in lyrics("br"):
+      br.replace_with("\n")
+
    fileName = join(kOutputDir, MakeFilename(albumName, trackName))
+
    Log("  {}".format(fileName))
+
    with open(fileName, "wt") as f:
-      lyric = u"\n".join([stanza.text for stanza in stanzas]).encode("UTF-8")
-      f.write(lyric)
+      f.write(lyrics.get_text().encode("UTF-8"))
 
 if __name__ == "__main__":
-   ProcessDiscography("wiki/Discography/Studio_Albums")
-   ProcessDiscography("wiki/Discography/John_Linnell")
-   ProcessDiscography("wiki/Discography/John_Flansburgh")
+   ProcessDiscography("")
