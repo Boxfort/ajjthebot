14bd5bd94512323a66b47a84b29aabc35fe37772
Sam Gaus <sam@gaus.co.uk>
2016-11-19 18:53:32 +0000

    Fix reply logging

--- server.js
+++ server.js
@@ -50,12 +50,12 @@ function replyer () {
     // If asked a question, respond with a lyric (hopefully).
     if (text.indexOf('?') > -1) {
       const prefix = `@${user.screen_name} `;
-      const reply = await pick(140 - prefix.length);
-      T.post('statuses/update', { status: `${prefix}${reply}`, in_reply_to_status_id: id_str });
+      const { song, tweet: reply } = await pick(140 - prefix.length);
+      T.post('statuses/update', { status: prefix + reply, in_reply_to_status_id: id_str });
 
-      console.log(`REPLY (@${user.screen_name})`);
+      console.log(`REPLY`);
       console.log('From:', song);
-      console.log('Tweeted:', tweet.replace(/\n/g, '\\n'));
+      console.log('Tweeted:', prefix + reply.replace(/\n/g, '\\n'));
       console.log();
     }
   });
